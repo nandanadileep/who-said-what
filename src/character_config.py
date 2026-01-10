67e4affa141717eae64f6e7f43463ffab5d23072
Nandana Dileep <110280757+nandanadileep@users.noreply.github.com>
2026-01-10 10:47:17 +0000

    setting up character normalisation

---
+++ src/character_config.py
@@ -0,0 +1,46 @@
+# Main recurring characters
+MAIN_CHARACTERS = {
+    "Sheldon",
+    "Leonard",
+    "Penny",
+    "Howard",
+    "Raj",
+    "Amy",
+    "Bernadette",
+    "Stuart"
+}
+
+# Relatives & close family
+RELATIVES = {
+    # Sheldon
+    "Mary Cooper",
+    "George Cooper",
+    "Missy Cooper",
+    "Meemaw",
+
+    # Leonard
+    "Beverly Hofstadter",
+    "Alfred Hofstadter",
+
+    # Penny
+    "Susan",
+    "Wyatt",
+
+    # Howard
+    "Debbie Wolowitz",
+
+    # Raj
+    "Mrs. Koothrappali",
+    "Dr. Koothrappali",
+
+    # Amy
+    "Mrs. Fowler",
+    "Mr. Fowler",
+
+    # Bernadette
+    "Mrs. Rostenkowski",
+    "Mr. Rostenkowski"
+}
+
+# Final allowed universe
+ALLOWED_CHARACTERS = MAIN_CHARACTERS.union(RELATIVES)
